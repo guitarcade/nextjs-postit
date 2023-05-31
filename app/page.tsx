79478b6f8488e98cd5225d2cb1881d36a442f8ce
Arik Cohen <me@arikcohen.com>
2023-05-31 16:09:00 -0400

    working on auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl p-5">Hello Next!</h1>
+      <h1 className="text-2xl">Hello Next!</h1>
     </main>
   );
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,11 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prismaClient } from "../../../prisma/client";
-
+import { client } from "../../../prisma/client";
 
 export const authOptions = {
-  adapter: PrismaAdapter(prismaClient),
+  adapter: PrismaAdapter(client),
+  secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
